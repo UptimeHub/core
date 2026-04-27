132691491f5715ab9cc97b40074b314455716256
xushnudbek-prog <xushnudbek756@gmail.com>
2026-04-27 17:09:00 +0500

    Set up project base: added `.gitignore`, updated `lombok` version, configured dependency management, and made minor code adjustments.

--- core-app/src/main/java/uz/uptimehub/coreapp/CoreAppApplication.java
+++ core-app/src/main/java/uz/uptimehub/coreapp/CoreAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoreAppApplication {
 
-    public static void main(String[] args) {
+     static void main(String[] args) {
         SpringApplication.run(CoreAppApplication.class, args);
     }
 
